69741a46cebf667e942071a49c1eb8fe523d5efb
antran1245 <antran1245@gmail.com>
2022-12-28 12:47:27 -0800

    countries card and filter working, style all for mobile

--- pages/api/countries.ts
+++ pages/api/countries.ts
@@ -2,8 +2,8 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { Countries } from "../interface/Countries";
 
 export default async function countries(req : NextApiRequest, res : NextApiResponse<Countries>) {
-  console.log(req.body)
-  const response = await fetch('https://restcountries.com/v3.1/all')
+  const {url} = JSON.parse(req.body)
+  const response = await fetch(`https://restcountries.com${url}`)
   const data = await response.json()
   res.status(200).json(data)
 }
